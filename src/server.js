ffd026acca4faef0301d33dcd9d03124beecedfc
Mathias <mathias@organicweb.fr>
2025-10-02 15:02:35 +0200

    init tp ci/cd

---
+++ src/server.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { hello } from "./index.js";
+
+const app = express();
+app.get("/", (_req, res) => res.json({ msg: hello() }));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on :${port}`));
